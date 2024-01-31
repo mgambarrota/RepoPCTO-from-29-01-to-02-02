992e8c01aa235a10b302dba5b319bfa6184088b2
Andrea Cotugno <andreacotu11@gmail.com>
2024-01-31 15:34:24 +0100

    Update DipendenteServiceImpl.java
    Co-Authored-By: salvatore-abello <107145304+salvatore-abello@users.noreply.github.com>

--- GestionaleDipedenti/src/main/java/it/betacom/GestionaleDipedenti/serviceImpl/DipendenteServiceImpl.java
+++ GestionaleDipedenti/src/main/java/it/betacom/GestionaleDipedenti/serviceImpl/DipendenteServiceImpl.java
@@ -1,15 +1,21 @@
 package it.betacom.GestionaleDipedenti.serviceImpl;
 
 import it.betacom.GestionaleDipedenti.model.DipendenteModel;
+import it.betacom.GestionaleDipedenti.repository.IDipendenteRepository;
 import it.betacom.GestionaleDipedenti.service.IDipendenteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
 public class DipendenteServiceImpl implements IDipendenteService {
+    @Autowired
+    private IDipendenteRepository repository;
+
     @Override
     public List<DipendenteModel> getAll() {
-        return null;
+        return repository.findAll(Sort.by(Sort.Order.desc("cognome")));
     }
 
     @Override
