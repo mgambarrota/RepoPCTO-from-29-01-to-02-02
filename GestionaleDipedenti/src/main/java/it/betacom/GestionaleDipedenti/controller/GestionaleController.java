aff0ccc10c7f59984b2d778a1440b071ffdb57a9
Salvatore Abello <salvatore.abello2005@gmail.com>
2024-02-01 13:20:51 +0100

    Update GestionaleController.java
    Co-Authored-By: Andrea Cotugno <115987247+IlCotugamer@users.noreply.github.com>

--- GestionaleDipedenti/src/main/java/it/betacom/GestionaleDipedenti/controller/GestionaleController.java
+++ GestionaleDipedenti/src/main/java/it/betacom/GestionaleDipedenti/controller/GestionaleController.java
@@ -1,15 +1,20 @@
 package it.betacom.GestionaleDipedenti.controller;
 
 
+import it.betacom.GestionaleDipedenti.config.APIError;
 import it.betacom.GestionaleDipedenti.config.APIResponse;
+import it.betacom.GestionaleDipedenti.config.ErrorDetail;
 import it.betacom.GestionaleDipedenti.model.DipendenteModel;
 import it.betacom.GestionaleDipedenti.repository.IDipendenteRepository;
 import it.betacom.GestionaleDipedenti.service.IDipendenteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -20,6 +25,55 @@ public class GestionaleController {
 
     @GetMapping(value = {"/getall"}, produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<APIResponse<List<DipendenteModel>>> getAll(){
-        
+        APIResponse.APIResponseBuilder<List<DipendenteModel>> builder = APIResponse.builder();
+        try{
+            List<DipendenteModel> dipendenti = dipendenteService.getAll();
+            if(!dipendenti.isEmpty()){
+                builder
+                        .descrizioneEsitoOperazione("Lista dei dipendenti" +
+                                                    "recuperata correttamente")
+                        .codiceEsitoOperazione(HttpStatus.OK.value())
+                        .oggettiRestituiti(dipendenti.size())
+                        .oggettiTotaliRestituiti(dipendenti.size())
+                        .payload(dipendenti);
+            }else{
+                builder
+                        .descrizioneEsitoOperazione("Lista dei dipendneti vuota")
+                        .codiceEsitoOperazione(HttpStatus.OK.value())
+                        .oggettiRestituiti(0)
+                        .oggettiTotaliRestituiti(0);
+            }
+
+            // Creazione dell'oggetto
+            APIResponse<List<DipendenteModel>> result = builder.build();
+
+            return ResponseEntity.status(HttpStatus.OK).body(result);
+
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                    APIResponse.builder()
+                            .codiceEsitoOperazione(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                            .descrizioneEsitoOperazione("Errore durante il recupero della " +
+                                    "lista dei dipendneti")
+                            .oggettiRestituiti(0)
+                            .oggettiTotaliRestituiti(0)
+                            .errori(
+                                    APIError.builder()
+                                            .timestamp(new Date())
+                                            .details(
+                                                    Collections.singletonList(
+                                                            ErrorDetail.builder()
+                                                                    .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+                                                                    .field("generico")
+                                                                    .source("repository.findAll()")
+                                                                    .message(e.getMessage()).build()
+                                                    )
+                                            )
+                                            .path("/public/gestioanle/getall").build()
+
+                            ).build()
+            );
+        }
+
     }
 }
