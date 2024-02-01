897d336b9da8773c6f2acc3f3c9f989bf42d2279
Michele Gambarrota <michele.gambarrota@eng.it>
2024-02-01 16:06:24 +0100

    init commit be

---
+++ GestionaleDipendenti/GestionaleDipendenti/src/main/java/it/betacom/GestionaleDipendenti/config/ApiError.java
@@ -0,0 +1,20 @@
+package it.betacom.GestionaleDipendenti.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiError {
+    @Builder.Default
+    private Date timestamp = new Date();
+    private String path;
+    private List<ErrorDetail> details;
+}
---
+++ GestionaleDipendenti/GestionaleDipendenti/src/main/java/it/betacom/GestionaleDipendenti/config/ApiResponse.java
@@ -0,0 +1,21 @@
+package it.betacom.GestionaleDipendenti.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.*;
+
+@Data
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse <T> {
+    private int codiceEsitoOperazione;
+    private String descrizioneEsitoOperazione;
+    private int oggettiRestituiti;
+    private int oggettiTotali;
+    private T payload;
+    @JsonInclude(Include.NON_NULL)
+    private ApiError errori;
+}
