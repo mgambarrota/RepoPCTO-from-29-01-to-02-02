98efe17fc1fd891f0558bccf8d8fe1619670bb7a
Eleirbag05 <gabrieletancrini@gmail.com>
2024-02-01 15:08:06 +0100

    be commit

--- GestionaleDipendenti/src/main/java/it/betacom/GestionaleDipendenti/controller/DipendentiController.java
+++ GestionaleDipendenti/src/main/java/it/betacom/GestionaleDipendenti/controller/DipendentiController.java
@@ -9,9 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -66,5 +65,42 @@ public class DipendentiController {
                            ).build()
            );
        }
+    }
+    @PostMapping(value= "/insert", consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<ApiResponse<DipendenteModel>> save(@RequestBody @Validated DipendenteModel model){
+        try{
+            DipendenteModel result = dipendenteService.saveOrUpdate(model);
+            return ResponseEntity.status(HttpStatus.OK).body(
+                    ApiResponse.<DipendenteModel>builder()
+                            .codiceEsitoOperazione(HttpStatus.OK.value())
+                            .descrizioneEsito("Inserimento del dipendente andato bene")
+                            .oggettiRestituiti(1)
+                            .oggettiTotali(1)
+                            .payload(result).build()
+            );
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                    ApiResponse.<DipendenteModel>builder()
+                            .codiceEsitoOperazione(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                            .descrizioneEsito("Errore inserimento")
+                            .oggettiRestituiti(0)
+                            .oggettiTotali(0)
+                            .errori(
+                                    ApiError.builder()
+                                            .timestamp(new Date())
+                                            .path("/insert")
+                                            .details(
+                                                Collections.singletonList(
+                                                        ErrorDetail.builder()
+                                                                .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+                                                                .field("generico")
+                                                                .source("repository.save()")
+                                                                .message(e.getMessage()).build()
+                                                )
+                                            ).build()
+
+                            ).build()
+            );
+        }
     }
 }
