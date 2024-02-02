847a6e8f451632290ee005c657ae4c5790e772fe
Salvatore Abello <salvatore.abello2005@gmail.com>
2024-02-02 11:41:31 +0100

    create geenric
    Co-Authored-By: Andrea Cotugno <115987247+IlCotugamer@users.noreply.github.com>

--- GestionaleDipendenti/fe-gestionaleDipendenti/src/app/services/generic-api.service.ts
+++ GestionaleDipendenti/fe-gestionaleDipendenti/src/app/services/generic-api.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ConsoleLoggingService } from './console-logging.service';
 import { GetOptions } from '../models/get-options.model';
-import { Observable, tap } from 'rxjs';
+import { Observable, catchError, tap } from 'rxjs';
 import { APIResponse } from '../models/api-response.model';
 import { CONST } from '../shared/constants';
 
@@ -14,17 +14,30 @@ export class GenericApiService {
     private _http: HttpClient,
     private _logging: ConsoleLoggingService
   ) {}
-  
+
   public getGenericService<T>(
     api: string, // endpoint
     opt?: GetOptions
-  ): Observable<APIResponse<T>>{
+  ): Observable<APIResponse<T>> {
     // Programmazione asincrona: non aspetto una risposta prima di inviare un altra richiesta
     return this._http
-    .get<APIResponse<T>>(api, opt || CONST.defaultOptions)
-    .pipe(
-      tap((result) => this._logging.printLog(result))
-    );
+      .get<APIResponse<T>>(api, opt || CONST.defaultOptions)
+      .pipe(tap((result) => this._logging.printLog(result)));
   }
 
+  public postGenericService<T>(
+    obj: T,
+    api: string,
+    opt?: GetOptions
+  ): Observable<APIResponse<T>> {
+    return this._http
+      .post<APIResponse<T>>(api, obj, opt || CONST.defaultOptions)
+      .pipe(
+        catchError((error) => {
+          this._logging.printLog(error);
+          throw Error();
+        }),
+        tap((result) => this._logging.printLog(result))
+      );
+  }
 }
