d95bfbd9bc3883c71ea34f9e4af0546ee24f9dd1
Salvatore Abello <salvatore.abello2005@gmail.com>
2024-02-02 12:06:09 +0100

    Update dipendente.service.ts
    Co-Authored-By: Andrea Cotugno <115987247+IlCotugamer@users.noreply.github.com>

--- GestionaleDipendenti/fe-gestionaleDipendenti/src/app/services/dipendente.service.ts
+++ GestionaleDipendenti/fe-gestionaleDipendenti/src/app/services/dipendente.service.ts
@@ -1,11 +1,42 @@
 import { Injectable } from '@angular/core';
+import { GenericApiService } from './generic-api.service';
+import { Router } from '@angular/router';
+import { CONST } from '../shared/constants';
+import { DipendenteBE, DipendenteFE } from '../models/dipendente.model';
+import { map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class DipendenteService {
+  dipendenti: DipendenteFE[];
+  
+  constructor(
+    private genericService: GenericApiService,
+    private router: Router
+  ) {}
 
-constructor() { }
-
+  getAll(){
+    return this.genericService.getGenericService<DipendenteBE[]>(`${CONST.API_URL}/getall`)
+    .pipe(
+      map((result) => {
+        this.dipendenti = result?.payload?.map((dip) => new DipendenteFE(dip)) || [];
+        return this.dipendenti;
+      })
+    )
+  } 
+  
+  save(dipendente: DipendenteFE){
+    let dipendenteBE: DipendenteBE = dipendente.toBE();
+    
+    return this.genericService.postGenericService<DipendenteBE>(
+      dipendenteBE, `${CONST.API_URL}/insert`
+    ).pipe(
+      map((result) => {
+        if(result.payload){
+          this.router.navigate(["/visualizza"]);
+        }
+      })
+    )
+  }
 }
